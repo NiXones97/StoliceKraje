00c044290888adce557bdf5900bf18f248340038
Kacper Szyjka <kacper.szyjka97@gmail.com>
2022-11-06 20:29:53 +0100

    end projekt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        Napisz program, który będzie pobierał od użytkownika stolicę i kraj dopóki nie wprowadzi „/”, (wykorzystaj pętlę do while)
+         następnie poproś użytkownika o podanie jednego z wcześniej wprowadzonych krajów i wyświetl odpowiadającą mu stolicę.
+         */
+
+        Map<String,String> names = new HashMap<>();
+
+        Scanner scan = new Scanner(System.in);
+        String stolica, kraj;
+
+        while (true) {
+
+            System.out.println("Podaj stolice :");
+            stolica = scan.next();
+
+            if (stolica.equals("/"))
+                break;
+
+            System.out.println("Podaj kraj :");
+            kraj = scan.next();
+
+            if (kraj.equals("/")){
+                break;
+            }
+            names.put(stolica,kraj);
+
+        }
+        System.out.println("Podaj klucz");
+        stolica = scan.next();
+        System.out.println(names.get(stolica));
+
+
+
+
+
+
+
+
+
+
+
     }
 }
